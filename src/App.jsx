0507a766432952a19165bbfd443a9b08687689b5
Alejo2304 <ojeda.alejandro.2304@gmail.com>
2025-06-28 00:14:17 -0500

    Fixed the component for the desktop version. Pending: Fix responsive design for the mobile version

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
 import ProductCard from "./components/ProductCard";
+import productImageDesktop from './assets/image-product-desktop.jpg'
+import productImageMobile from './assets/image-product-mobile.jpg'
 
 export default function App() {
   return (
-    <ProductCard />
+    <div className="flex justify-center items-center min-h-screen min-w-screen bg-cream">
+      <div className="max-w-[30vw] max-h-[60vh]">
+        <ProductCard 
+        pType='PERFUME'
+        pName='Gabrielle Essence Eau De Parfum'
+        pDescription='A floral, solar and voulptuos interpretation composed by Olivier Polge, Perfumer-Creator for the House of CHANNEL'
+        pNewPrice='149.99'
+        pOldPrice='169.99' 
+        pImageMobile={productImageMobile}
+        pImageDesktop={productImageDesktop}
+        />
+      </div>
+    </div>
   );
 }
--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,12 +1,45 @@
 import cartIcon from '../assets/icon-cart.svg';
 
+function ProductImage({pMobileImage, pDesktopImage, altText}){
+    return(    
+        <div className="md:w-full md:h-full">
+        {/* Imagen para móviles: visible por defecto, oculta en md y superior */}
+        <img
+            src={pMobileImage}
+            alt={altText}
+            className="w-full h-full object-cover md:hidden"
+        />
+        {/* Imagen para escritorio: oculta por defecto, visible en md y superior */}
+        <img
+            src={pDesktopImage}
+            alt={altText}
+            className="w-full h-full object-cover hidden md:block"
+        />
+        </div>
+    );
+}
+
+function ProductDetails({productType, productName, productDescription}){
+    return (
+        <div className="flex-col px-5" >
+            <header>
+                <p className="text-grey text-md  tracking-wider">{productType}</p>
+                <h1 className="text-black font-headings text-3xl py-3">{productName}</h1>
+            </header>
+            <section>
+                <p className="text-grey font-body text-sm">{productDescription}</p>
+            </section>
+        </div>
+    );
+}
+
 function Price({newPrice, oldPrice}){
     return(
-        <div class="inline-flex py-2 px-8 items-center justify-center space-x-3">
-            <h1 class="text-green-700 font-headings text-2xl flex-2/3">
+        <div className="inline-flex flex-nowrap py-4 px-15 items-center justify-center space-x-5">
+            <h1 className="text-green-500 font-headings text-3xl flex-2/3">
                 ${newPrice}
             </h1>
-            <h3 class="text-grey line-through text-xs flex-1/3">
+            <h3 className="text-grey line-through text-xs flex-1/3">
                 ${oldPrice}
             </h3>
         </div>
@@ -16,19 +49,27 @@ function Price({newPrice, oldPrice}){
 
 function AddToCartButton({iconPath}){
     return (
-        <button class="bg-green-500 text-white  text-xs font-bold py-4 px-8 rounded inline-flex items-center justify-center space-x-2">
-                    <img src={iconPath} alt="Cart Icon" class="w-3 h-3"></img>
-                    <p> Add to Cart </p>
-        </button>
+        <div className="flex-col justify-center items-center">
+            <button className="bg-green-500 text-white  text-xs font-bold py-3 px-18 rounded inline-flex items-center justify-center space-x-2">
+                        <img src={iconPath} alt="Cart Icon" className="w-3 h-3"></img>
+                        <p> Add to Cart </p>
+            </button>
+        </div>
     );
 }
 
 
-export default function ProductCard(){
+export default function ProductCard({pType, pName, pDescription, pNewPrice, pOldPrice, pImageMobile, pImageDesktop}){
     return(
-        <div class="inline-flex">
-            <Price newPrice="149.99" oldPrice="169.99"></Price>
-            <AddToCartButton iconPath={cartIcon} />
-        </div>
+        <article className="flex md:flex-row bg-white rounded-lg shadow-lg overflow-hidden">
+            <div className = 'flex-1/2 border'>
+                <ProductImage pMobileImage={pImageMobile} pDesktopImage={pImageDesktop} altText={pName} />
+            </div>
+            <div className='flex flex-1/2 flex-col justify-center items-center border py-2'>
+                <ProductDetails productType={pType} productName={pName} productDescription={pDescription} />
+                <Price newPrice={pNewPrice} oldPrice={pOldPrice} />
+                <AddToCartButton iconPath={cartIcon} />
+            </div>
+        </article>
     );
 }
